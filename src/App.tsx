750dbe9fe09f3c009e55a51c7ea4a94cca00a772
Moshe <m0504136536@gmail.com>
2024-10-28 11:10:52 +0200

    create class name

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+import Add from "./components/Add";
+import List from "./components/List";
+import todoModel from "./Models/todoModel";
 
 export default function App() {
+  const [todos, setTodos] = useState<todoModel[]>([]);
   return (
-    <div>
-      App
+    <div className="app">
+      <Add />
+      <List />
     </div>
   )
 }
---
+++ src/components/Item.tsx
@@ -0,0 +1,8 @@
+
+export default function Item() {
+  return (
+    <div className="item">
+      
+    </div>
+  )
+}
